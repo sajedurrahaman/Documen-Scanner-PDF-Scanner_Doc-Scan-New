7e8ad09ebf5d394bc548ae3a3063dec18361664a
DAV-2 <appgraphicscycle2@gmail.com>
2026-09-17 13:15:57 +0600

    update v2.0.8

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/opt/homebrew/share/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/rajibhossain/Shahjalal/app/Documen-Scanner-PDF-Scanner_Doc-Scan"
-export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=/Users/rajibhossain/Shahjalal/app/Documen-Scanner-PDF-Scanner_Doc-Scan/macos/Flutter/ephemeral/Packages/.packages/FlutterFramework"
+export "FLUTTER_ROOT=/Users/mdsajedurrahman/App Development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/mdsajedurrahman/App Development/Test/Documen-Scanner-PDF-Scanner_Doc-Scan"
+export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=/Users/mdsajedurrahman/App Development/Test/Documen-Scanner-PDF-Scanner_Doc-Scan/macos/Flutter/ephemeral/Packages/.packages/FlutterFramework"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=2.0.6"
-export "FLUTTER_BUILD_NUMBER=26"
+export "FLUTTER_BUILD_NAME=2.0.8"
+export "FLUTTER_BUILD_NUMBER=28"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
